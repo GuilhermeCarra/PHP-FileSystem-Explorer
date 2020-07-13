9fce17e0528b1b2a9798050bf064cc958d119017
guilherme-carra <gui_gc@hotmail.com>
2020-07-13 18:31:39 +0200

    implemented function to convert bytes

--- filesFunctions.php
+++ filesFunctions.php
@@ -16,19 +16,34 @@ switch ($_POST['operation']) {
 }
 
 function showFolder() {
+
+    // Getting all folders names
     $dirs = array_filter(scandir($_POST["actualDir"]), function($item) {
         return is_dir($_POST["actualDir"] ."/". $item);
     });
     $dirs = array_diff($dirs, array('.', '..'));
 
+
+    // Getting all files names
     $files = array_filter(scandir($_POST["actualDir"]), function($item) {
         return !is_dir($_POST["actualDir"] ."/". $item);
     });
 
+    // Getting all files sizes
+    $filesSize = array();
+    $filesArray = scandir($_POST["actualDir"]);
+    foreach($filesArray as $item) {
+        if (!is_dir($_POST["actualDir"] ."/". $item)){
+            $bytes = filesize($_POST["actualDir"].$item);
+            $filesSize[] = bytesConvert($bytes);
+        }
+    }
+
     $dirs = array_values($dirs);
     $files = array_values($files);
+    $filesSize = array_values($filesSize);
 
-    $content = array(json_encode($dirs),json_encode($files));
+    $content = array(json_encode($dirs),json_encode($files),json_encode($filesSize));
 
     echo json_encode($content);
 
@@ -44,6 +59,19 @@ function renameFile() {
 
 function deleteFile() {
 
+}
+
+function bytesConvert($bytes) {
+    if ($bytes >= 1073741824) {
+        $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+    }
+    elseif ($bytes >= 1048576) {
+        $bytes = number_format($bytes / 1048576, 2) . ' MB';
+    }
+    else {
+        $bytes = number_format($bytes / 1024, 2) . ' KB';
+    }
+    return $bytes;
 }
 
 ?>
