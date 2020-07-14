692c925cf705c0a9a7e1ef03a64403e098525ead
guilherme-carra <gui_gc@hotmail.com>
2020-07-14 12:37:07 +0200

    eliminated hard coded files

--- index.php
+++ index.php
@@ -42,35 +42,15 @@
             </div>
             <div class="col-md-6 animated fadeInRight">
                 <div class="row">
-                    <div class="col-lg-12" id="folder">
+                    <div class="col-lg-12">
                         <nav class="navbar navbar-light bg-light">
-                            <a class="navbar-brand ml-5">Name</a>
+                            <a class="navbar-brand">Name</a>
                             <form class="form-inline">
-                                <a class="navbar-brand mr-5">Size</a>
+                                <a class="navbar-brand">Size</a>
                             </form>
                         </nav>
-                        <ul class="list-unstyled">
-                            <li class="media">
-                                <img src="assets/img/excel-2.png" class="mr-3 ml-1 mt-2 mb-1" alt="..." height="50px"
-                                    width="50px">
-                                <div class="media-body">
-                                    <h5 class="mt-0 mb-1 mt-3">List-based media object</h5>
-                                </div>
-                            </li>
-                            <li class="media my-4">
-                                <img src="assets/img/excel-2.png" class="mr-3 ml-1 mt-2 mb-1" alt="..." height="50px"
-                                    width="50px">
-                                <div class="media-body">
-                                    <h5 class="mt-0 mb-1 mt-3">List-based media object</h5>
-                                </div>
-                            </li>
-                            <li class="media">
-                                <img src="assets/img/excel-2.png" class="mr-3 ml-1 mt-2 mb-1" alt="..." height="50px"
-                                    width="50px">
-                                <div class="media-body">
-                                    <h5 class="mt-0 mb-1 mt-3">List-based media object</h5>
-                                </div>
-                            </li>
+                        <ul class="list-unstyled" id="folder">
+                            <!-- Archives / Folder printed dynamically -->
                         </ul>
                     </div>
                 </div>
@@ -109,7 +89,7 @@
                 <i class="fa fa-font"></i> Rename
             </div>
         </div>
-
+    </div>
 
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
