00559ad2e2e3eea7bc639893698e0a3503549f81
guilherme-carra <gui_gc@hotmail.com>
2020-07-15 20:03:19 +0200

    indentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,38 @@
 Array.prototype.forEach.call(
     document.querySelectorAll(".file-upload__button"),
     function(button) {
-      const hiddenInput = button.parentElement.querySelector(
+    const hiddenInput = button.parentElement.querySelector(
         ".file-upload__input"
-      );
-      const label = button.parentElement.querySelector(".file-upload__label");
-      const defaultLabelText = "No file(s) selected";
-  
-      // Set default text for label
-      label.textContent = defaultLabelText;
-      label.title = defaultLabelText;
-  
-      button.addEventListener("click", function() {
+    );
+    const label = button.parentElement.querySelector(".file-upload__label");
+    const defaultLabelText = "No file(s) selected";
+
+    // Set default text for label
+    label.textContent = defaultLabelText;
+    label.title = defaultLabelText;
+
+    button.addEventListener("click", function() {
         hiddenInput.click();
-      });
-  
-      hiddenInput.addEventListener("change", function() {
+    });
+
+    hiddenInput.addEventListener("change", function() {
         const filenameList = Array.prototype.map.call(hiddenInput.files, function(
-          file
+        file
         ) {
-          return file.name;
+        return file.name;
         });
-  
+
         label.textContent = filenameList.join(", ") || defaultLabelText;
         label.title = label.textContent;
-      });
+    });
     }
-  );
+);
 
-  document.getElementById("logofile").addEventListener("click",function(){
-      location.reload();
-  });
+document.getElementById("logofile").addEventListener("click",function(){
+    location.reload();
+});
 
-  
+
 
   // TIME TO FINISH
 /* Timer to finish 
